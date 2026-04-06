77f11e97cd107db68b64c88dcc1c8e12be1c1a1f
Voaides Robert <voaides.r@yahoo.com>
2026-04-06 13:22:14 +0300

    added way to construct individual

--- app/Main.hs
+++ app/Main.hs
@@ -27,26 +27,45 @@ data Individual = Individual
   deriving (Show, Eq)
 
 -- math helpers
+_bitsToInt :: [Bit] -> Int
+_bitsToInt = foldl (\acc bit -> 2 * acc + bit) 0
 
 chromLength :: (Double, Double) -> Int -> Int
 chromLength (a, b) p = ceiling . logBase 2 $ (b - a) * 10 ^^ p
 
 decode :: Config -> Chrom -> Double
-decode = undefined
+decode cfg chr =
+  let (a, b) = domain cfg
+      l = chromLen cfg
+      val = fromIntegral . _bitsToInt $ chr
+   in a + (val * (b - a) / (2 ^ l - 1))
+
+calcFitness :: Config -> Double -> Double
+calcFitness cfg val =
+  let (a, b, c) = coeffs cfg
+   in a * val ** 2 + b * val + c
+
+makeIndividual :: Config -> Chrom -> Individual
+makeIndividual cfg chr =
+  let val = decode cfg chr
+      fit = calcFitness cfg val
+   in Individual chr val fit
 
 main :: IO ()
 main = do
   -- Function: -x^2 + x + 2, Domain: [-1, 2], Precision: 6
+  let d = (-2.0, 2.0)
+  let p = 3
   let testConfig =
         Config
           { popSize = 20,
-            domain = (-1.0, 2.0),
+            domain = d,
             coeffs = (-1.0, 1.0, 2.0),
-            precision = 6,
+            precision = p,
             crossProb = 0.25,
             mutProb = 0.01,
             numSteps = 50,
-            chromLen = 0 -- We will calculate this next
+            chromLen = chromLength d p
           }
 
   let l = chromLength (domain testConfig) (precision testConfig)
@@ -54,3 +73,9 @@ main = do
   printf "Testing Chromosome Length:\n"
   printf "Domain: %s, Precision: %d\n" (show $ domain testConfig) (precision testConfig)
   printf "Calculated Length (L): %d bits\n\n" l
+
+  printf "%d\n" (_bitsToInt [1, 0, 1, 0])
+
+  printf "%d\n" (chromLen testConfig)
+
+  printf "%f\n" (decode testConfig [1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1])
